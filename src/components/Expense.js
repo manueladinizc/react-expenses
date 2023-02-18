2614caf59b391a3c79d1d0952c902da26ad43642
manuela.cavalcanti <manuela.cavalcanti@rebase.com.br>
2023-02-17 22:24:09 -0300

    adiciona expenses component

--- src/components/Expense.js
+++
@@ -1,3 +0,0 @@
-import ExpenseDate from './ExpenseDate'
-import Expense from './ExpenseItem'
-
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,13 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-
+ 
   return (
   <div className="expense-item">
     <ExpenseDate date={props.date}/>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
-    </div>    
+    </div>
       <div className="expense-item__price">{props.amount}</div>
   </div>
   );
