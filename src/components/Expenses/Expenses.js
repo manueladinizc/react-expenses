6b2b09ec720b6f0c7dc2d936fac5f0ee30c9da62
manueladinizc <manuela.mdc@gmail.com>
2023-04-24 22:44:56 -0300

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,6 +14,24 @@ import ExpensesFilter from './ExpensesFilter';
     console.log('Expenses.js '+ selectYear);    
     setFilteredYear(selectYear);
   }
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0 ){
+      expensesContent = filteredExpenses.map((expense) => ( 
+      <ExpenseItem 
+      title={expense.title} 
+      amount={expense.amount}
+      date={expense.date}
+      />
+    ));
+  }
+  
+
   
   return (
     <div> 
@@ -21,21 +39,7 @@ import ExpensesFilter from './ExpensesFilter';
         <ExpensesFilter 
         selected={filteredYear} 
         onChangeFilter ={filterChangeHandler}/>
-        <ExpenseItem 
-        title={props.items[0].title} 
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-        />
-        <ExpenseItem 
-        title={props.items[1].title} 
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-        />
-        <ExpenseItem 
-        title={props.items[2].title} 
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-        />
+        {expensesContent}
     </Card>
   </div>
 
